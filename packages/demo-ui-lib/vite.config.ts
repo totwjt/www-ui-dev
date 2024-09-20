37fcc0cc837f717211b84088b9a6ab1e4e61f84d
totwjt <totwjt@sina.cn>
2024-09-20 13:35:03 +0800

    tool

--- packages/demo-ui-lib/vite.config.ts
+++ packages/demo-ui-lib/vite.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
     },
     outDir: path.resolve(__dirname, '../../lib'),
     rollupOptions: {
-      external: ['vue'],
+      external: ['vue', /^@demo-ui-lib\/(?!tool)/],
       output: {
         globals: {
           vue: 'Vue'
